daafd0e76516087b3c15038f5e69d7e4fcad2c1b
JTK <red.mailhub@gmail.com>
2018-02-21 22:11:06 +0800

    fix timestamp with time delta

--- news/views.py
+++ news/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+import datetime
 
 import webhoseio
+
 
 
 def index(request):
     # Create your views here.
     webhoseio.config(token="8ebd85ed-94da-4ae1-9bd2-def0554ceb64")
+    time_now = datetime.datetime.now() 
+    time_30_days_before =  time_now - datetime.timedelta(days=30)
+    ts_30_days_before = time_30_days_before.timestamp()
     query_params = {
     "q": "(site:bloomberg.com OR site:reuters.com) AND ('mapletree' OR 'capitaland' OR 'Keppel')",
-    "ts": "1516541338427",
+    "ts": ts_30_days_before,
     "sort": "published"
     }
     output = webhoseio.query("filterWebContent", query_params)
