623ab98c8e48f756687863b48105f42c07d7c3fb
JTK <red.mailhub@gmail.com>
2018-02-21 22:17:32 +0800

    fix split of fullstop for cutText filter

--- news/templatetags/news_extras.py
+++ news/templatetags/news_extras.py
@@ -5,7 +5,7 @@ register = template.Library()
 @register.filter(name='cutText')
 def cutText(value):
     """Removes all values of arg from the given string"""
-    return value.split('.', 1)[0]
+    return value.split('. ', 1)[0]
 
 @register.filter(name='cutDate')
 def cutText(value):
